6d68c08c3dfa6bdd2742659a7a5a0703ac7204a6
Femi Olasanmi (folasanm) <folasanm@cisco.com>
2022-04-04 09:24:27 -0400

    update to the netconf dir

--- network_device_apis/netconf/device_info.py
+++ network_device_apis/netconf/device_info.py
@@ -1,18 +1,9 @@
-#! /usr/bin/env python
 """
-Learning Series: Network Programmability Basics
-Author: Hank Preston <hapresto@cisco.com>
-
 device_info.py
 Illustrate the following concepts:
 - Store device info for Sandbox Infrastructure used 
   in examples
 """
-
-__author__ = "Hank Preston"
-__author_email__ = "hapresto@cisco.com"
-__copyright__ = "Copyright (c) 2016 Cisco Systems, Inc."
-__license__ = "MIT"
 
 # DevNet Always-On IOS XE on CSR Recommended Code Sandbox Device
 # https://devnetsandbox.cisco.com/RM/Diagram/Index/27d9747a-db48-4565-8d44-df318fce37ad?diagramType=Topology
--- network_device_apis/netconf/example2.py
+++ network_device_apis/netconf/example2.py
@@ -1,10 +1,4 @@
-#! /usr/bin/env python
 """
-Learning Series: Network Programmability Basics
-Module: Network Device APIs
-Lesson: Goodbye SNMP hello NETCONF
-Author: Hank Preston <hapresto@cisco.com>
-
 example2.py
 Illustrate the following concepts:
 - Send <get> to retrieve config and state data
@@ -12,26 +6,28 @@ Illustrate the following concepts:
 - Report back current state of interface
 """
 
-__author__ = "Hank Preston"
-__author_email__ = "hapresto@cisco.com"
-__copyright__ = "Copyright (c) 2016 Cisco Systems, Inc."
-__license__ = "MIT"
-
 from device_info import ios_xe1
 from ncclient import manager
 import xmltodict
+import xml.dom.minidom
 
 # NETCONF filter to use
 netconf_filter = open("filter-ietf-interfaces.xml").read()
 
 if __name__ == '__main__':
-    with manager.connect(host=ios_xe1["address"], port=ios_xe1["port"],
-                         username=ios_xe1["username"],
-                         password=ios_xe1["password"],
-                         hostkey_verify=False) as m:
+    with manager.connect(
+                        host=ios_xe1["address"], 
+                        port=ios_xe1["port"],
+                        username=ios_xe1["username"],
+                        password=ios_xe1["password"],
+                        hostkey_verify=False
+                         ) as m:
 
         # Get Configuration and State Info for Interface
         netconf_reply = m.get(netconf_filter)
+
+        print (netconf_reply)
+        #netconf_reply = m.get_config(source = 'running', filter = netconf_filter)
 
         # Process the XML and store in useful dictionaries
         intf_details = xmltodict.parse(netconf_reply.xml)["rpc-reply"]["data"]
