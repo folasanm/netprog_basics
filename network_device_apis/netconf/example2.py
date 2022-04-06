e94055c459f1aa0469bc03b0fd6d426f047f0e21
Femi Olasanmi (folasanm) <folasanm@cisco.com>
2022-04-06 16:16:51 -0400

    trying to figure out what the netconf code not working on version 17 csr

--- network_device_apis/netconf/example2.py
+++ network_device_apis/netconf/example2.py
@@ -5,36 +5,38 @@ Illustrate the following concepts:
 - Process and leverage XML within Python
 - Report back current state of interface
 """
+
 from device_info import csr1
 from ncclient import manager
 import xmltodict
+import xml.dom.minidom
 
 
 # NETCONF filter to use
 netconf_filter = open("filter-ietf-interfaces.xml").read()
 
-if __name__ == '__main__':
-    with manager.connect(host=csr1["address"], port=csr1["port"],
-                         username=csr1["username"],
-                         password=csr1["password"],
-                         hostkey_verify=False) as m:
-    
+with manager.connect(host =csr1 ["address"], 
+                        port =csr1 ["port"],
+                        username =csr1 ["username"],
+                        password =csr1 ["password"],
+                        hostkey_verify=False,) as m:
 
-        # Get Configuration and State Info for Interface
-        netconf_reply = m.get(netconf_filter)
 
-        # Process the XML and store in useful dictionaries
-        intf_details = xmltodict.parse(netconf_reply.xml)["rpc-reply"]["data"]
-        intf_config = intf_details["interfaces"]["interface"]
-        intf_info = intf_details["interfaces-state"]["interface"]
+    # Get Configuration and State Info for Interface
+    netconf_reply = m.get(('subtree',  netconf_filter))
 
-"""
-        print("")
-        print("Interface Details:")
-        print("  Name: {}".format(intf_config["name"]["#text"]))
-        print("  Description: {}".format(intf_config["description"]))
-        print("  Type: {}".format(intf_config["type"]["#text"]))
-        print("  MAC Address: {}".format(intf_info["phys-address"]))
-        print("  Packets Input: {}".format(intf_info["statistics"]["in-unicast-pkts"]))
-        print("  Packets Output: {}".format(intf_info["statistics"]["out-unicast-pkts"]))
-"""
+    print(netconf_reply)
+    # Process the XML and store in useful dictionaries
+    #intf_details = xmltodict.parse(netconf_reply.xml)["rpc-reply"]["data"]
+    intf_details = xml.dom.minidom.parseString(netconf_reply)["rpc-reply"]["data"]
+    intf_config = intf_details["interfaces"]["interface"]
+    intf_info = intf_details["interfaces-state"]["interface"]
+
+    print("")
+    print("Interface Details:")
+    print("  Name: {}".format(intf_config["name"]["#text"]))
+    print("  Description: {}".format(intf_config["description"]))
+    print("  Type: {}".format(intf_config["type"]["#text"]))
+    print("  MAC Address: {}".format(intf_info["phys-address"]))
+    print("  Packets Input: {}".format(intf_info["statistics"]["in-unicast-pkts"]))
+    print("  Packets Output: {}".format(intf_info["statistics"]["out-unicast-pkts"]))
