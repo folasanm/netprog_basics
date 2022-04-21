460d1a526320a5af57d31fcea67b9cdcbb83a4c4
Femi Olasanmi (folasanm) <folasanm@cisco.com>
2022-04-21 10:13:12 -0400

    Pre working NETCONF for my lab

--- network_device_apis/netconf/example2.py
+++ network_device_apis/netconf/example2.py
@@ -6,36 +6,45 @@ Illustrate the following concepts:
 - Report back current state of interface
 """
 
-from device_info import csr1
+from device_info import  csr1
 from ncclient import manager
 import xmltodict
-# import xml.dom.minidom
+import xml.dom.minidom
+from pprint import pp, pprint
 
 # NETCONF filter to use
 netconf_filter = open("filter-ietf-interfaces.xml").read()
 
-with manager.connect(host =csr1 ["address"], 
-                        port =csr1 ["port"],
-                        username =csr1 ["username"],
-                        password =csr1 ["password"],
-                        hostkey_verify=False,) as m:
+
+if __name__ == '__main__':
+    with manager.connect(host = csr1 ["address"], 
+                            port = csr1 ["port"],
+                            username = csr1 ["username"],
+                            password = csr1 ["password"],
+                            hostkey_verify=False,) as m:
 
 
-    # Get Configuration and State Info for Interface
-    netconf_reply = m.get_config('running')
+        # Get Configuration and State Info for Interface
+        netconf_reply = m.get(netconf_filter)
+        #print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
 
-    print(netconf_reply)
-    # Process the XML and store in useful dictionaries
-    intf_details = xmltodict.parse(netconf_reply.xml)["rpc-reply"]["data"]
-    #intf_details = xml.dom.minidom.parseString(netconf_reply)["rpc-reply"]["data"]
-    intf_config = intf_details["interfaces"]["interface"]
-    intf_info = intf_details["interfaces-state"]["interface"]
+        intf_details = xmltodict.parse(netconf_reply.xml)["rpc-reply"]["data"]  
+        intf_config = intf_details["interfaces"]["interface"]
+        intf_info = intf_details["interfaces-state"]["interface"]
 
-    print("")
-    print("Interface Details:")
-    print("  Name: {}".format(intf_config["name"]["#text"]))
-    print("  Description: {}".format(intf_config["description"]))
-    print("  Type: {}".format(intf_config["type"]["#text"]))
-    print("  MAC Address: {}".format(intf_info["phys-address"]))
-    print("  Packets Input: {}".format(intf_info["statistics"]["in-unicast-pkts"]))
-    print("  Packets Output: {}".format(intf_info["statistics"]["out-unicast-pkts"]))
+        #pprint(intf_details)
+        #pprint(intf_config)
+        #pprint(intf_info)
+
+        for interface in intf_config:
+            print("")
+            print("Interface Details:")
+            print("  Name: {}".format(interface["name"]))
+            print("  Link Enabled: {}".format(interface["enabled"]))
+
+        for state in intf_info:
+            # print("  Description: {}".format(intf_config["description"]))
+            print("  MAC Address: {}".format(state["phys-address"]))
+            print("  Packets Input: {}".format(state["statistics"]["in-unicast-pkts"]))
+            print("  Packets Output: {}".format(state["statistics"]["out-unicast-pkts"]))
+    
